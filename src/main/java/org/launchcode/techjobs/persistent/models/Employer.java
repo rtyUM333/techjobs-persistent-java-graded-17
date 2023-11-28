833896ab67e147a24d1c1aee270552caa31312d5
RP <rp@RPdeMacBook-Pro-2.local>
2023-11-28 12:40:21 -0600

    Finished Task 2 but failing 1 test - SQL query

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -10,5 +10,16 @@ public class Employer extends AbstractEntity {
 
     @NotBlank(message = "Location is required")
     @Size(min = 3, max = 100, message = "Location must be between 3 and 100 characters")
+
     private String location;
+
+    public Employer () {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
