cd68a6e93e0ff7c44b221f4e422e426b10c5b757
RP <rp@RPdeMacBook-Pro-2.local>
2023-12-02 15:20:56 -0600

    finished task 2 and 3 with tests passing

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,8 +1,13 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -12,6 +17,11 @@ public class Employer extends AbstractEntity {
     @Size(min = 3, max = 100, message = "Location must be between 3 and 100 characters")
 
     private String location;
+
+    @OneToMany
+    @JoinColumn (name = "employer_id")
+
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer () {}
 
